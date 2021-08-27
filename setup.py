b7b045d92e8db3a9d7f45ea6593e57c73ae8ec11
VitalDB <lucid80@gmail.com>
2021-08-28 08:47:22 +0900

    0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
  
 setuptools.setup(
     name="pyvital",
-    version="0.0.2",
+    version="0.0.4",
     author="VitalLab",
     author_email="vital@snu.ac.kr",
     description="Python Libray for Biosignal Analysis",
@@ -10,7 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/vitaldb/pyvital",
     install_requires=['numpy','scipy'],
-    packages=setuptools.find_packages(),
+    packages=['pyvital', 'pyvital.filters'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- test.py
+++ test.py
@@ -3,4 +3,3 @@ vals = vitaldb.load_case(caseid=1, tnames=['SNUADC/ART'], interval=1/100)
 
 import pyvital.filters.abp_ppv as f
 print(f.cfg)
-
